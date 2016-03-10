a8f774e73a16a7f041281a58446a90225cf8b4ba
jnjeG <qchycjj@126.com>
2016-03-11 01:23:17 +0800

    jdo code

--- src/main/java/spring/orm/hibernate/peculiar/xTest/service/impl/XServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package spring.orm.hibernate.peculiar.xTest.service.impl;
-
-import spring.jdbc.common.service.UrUserService;
-
-/**
- * Created by chenjunjie on 16/2/24.
- */
-public class XServiceImpl implements UrUserService {
-}
--- src/main/java/spring/orm/jdo/peculiar/xTest/service/XService.java
+++
@@ -1,7 +0,0 @@
-package spring.orm.jdo.peculiar.xTest.service;
-
-/**
- * Created by chenjunjie on 16/2/24.
- */
-public interface XService {
-}
