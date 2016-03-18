1aff47e12e88e5f99c449a803ad26b4941a0010a
jneG <jnje123ysdy>
2016-03-18 14:50:28 +0800

    add

--- src/main/java/expl/util/ArrayListTest.java
+++ src/main/java/expl/util/ArrayListTest.java
@@ -3,6 +3,8 @@ package expl.util;
 import org.junit.Assert;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by chenjunjie on 16/3/17.
@@ -17,5 +19,22 @@ public class ArrayListTest {
         ArrayList<String> l = new ArrayList<String>();
         boolean addSuccess = l.addAll(addList);
         Assert.assertEquals(true,addSuccess);
+    }
+
+
+    public static void setTest(){
+        List<Object> l = new ArrayList<Object>(Arrays.asList("foo", "bar"));
+        l.set(0, new Object());
+    }
+
+    public static void cloneTest(){
+        ArrayList<Object> l = new ArrayList<Object>(Arrays.asList("foo", "bar"));
+        ArrayList<Object> clone =(ArrayList<Object>) l.clone();
+        System.out.println(clone.size());
+    }
+
+    public static void getTest(){
+        List<Object> l = new ArrayList<Object>(Arrays.asList("foo", "bar"));
+        Object o = l.get(-1);
     }
 }
