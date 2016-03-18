09b9d34943a9659f48427317348d369c391c06e7
jneG <jnje123ysdy>
2016-03-18 10:10:13 +0800

    当设值注入与构造注入同时存在时，先执行设置注入，在执行构造注入。

---
+++ src/main/java/expl/util/ArraysTest.java
@@ -0,0 +1,50 @@
+package expl.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/3/17.
+ */
+public class ArraysTest {
+
+    public static void main(String[] args) {
+//        copyOfTest();
+        copyOfTestV2();
+    }
+
+    public static void copyOfTest(){
+        List<Object> l = new ArrayList<Object>(Arrays.asList("foo", "bar"));
+        String[] strings = Arrays.copyOf(l.toArray(), l.size(), String[].class);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+        /*Object[] strings = Arrays.copyOf(l.toArray(), l.size(), Object[].class);
+        for (Object string : strings) {
+            System.out.println(string);
+        }*/
+    }
+
+    public static void copyOfTestV2(){
+        // initializing unsorted short array
+        Short shortArr[] = new Short[]{5, 2, 15, 52, 10};
+
+        // copy the array into another Number array
+        Number[] arr2 = Arrays.copyOf(shortArr, 5, Number[].class);
+        // let us print all the elements available in list
+        System.out.println("The copied number array is:");
+        for (Number number : arr2) {
+            System.out.println("Number = " + number);
+        }
+
+        //uncomment the code below
+       /* Object[] arr2 = Arrays.copyOf(shortArr, 5, Object[].class);
+        System.out.println("The copied number array is:");
+        for (Object number : arr2) {
+            System.out.println("Number = " + number);
+        }*/
+    }
+
+
+}
