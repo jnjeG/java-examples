f9cc349bad96425a35a9819b3ef86659ee3fa860
jnjeG <qchycjj@126.com>
2016-03-20 16:18:50 +0800

    commit AccessController test

---
+++ src/main/java/expl/java/security/accesscontroller/util/FileUtil.java
@@ -0,0 +1,34 @@
+package expl.java.security.accesscontroller.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.AccessControlException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+public class FileUtil {
+    // 工程 A 执行文件的路径
+    private final static String FOLDER_PATH = "/Users/chenjunjie/code/git/java-examples/target/classes/expl/java/security/accesscontroller/resources";
+
+    public static void makeFile(String fileName) {
+        try {
+            // 尝试在工程 A 执行文件的路径中创建一个新文件
+            File fs = new File(FOLDER_PATH + File.separator + fileName);
+            fs.createNewFile();
+        } catch (AccessControlException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void doPrivilegedAction(final String fileName) {
+        // 用特权访问方式创建文件
+        AccessController.doPrivileged(new PrivilegedAction<String>() {
+            public String run() {
+                makeFile(fileName);
+                return null;
+            }
+        });
+    }
+}
