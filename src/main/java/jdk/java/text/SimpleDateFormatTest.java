34128bb496f2da86c1c6604782de9eb27997d78c
jnjeG <qchycjj@126.com>
2016-05-02 20:54:38 +0800

    add test code

---
+++ src/main/java/jdk/java/text/SimpleDateFormatTest.java
@@ -0,0 +1,39 @@
+package jdk.java.text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by chenjunjie on 16/5/2.
+ */
+public class SimpleDateFormatTest {
+    public static void main(String[] args) throws IOException, ParseException {
+        // TODO Auto-generated method stub
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("请键入日期（如：2008-8-8）：");
+        String str = br.readLine();
+        Date date = dateFormatter.parse(str);
+        dateFormatter.applyPattern("D");//
+        System.out.println("一年中的第几天：" + dateFormatter.format(date));
+
+        dateFormatter.applyPattern("d");
+        System.out.println("一个月中的第几天：" + dateFormatter.format(date));
+
+        dateFormatter.applyPattern("w");
+        System.out.println("一年中的第几周：" + dateFormatter.format(date));
+
+        dateFormatter.applyPattern("W");
+        System.out.println("一个月中的第几周：" + dateFormatter.format(date));
+
+        dateFormatter.applyPattern("E");
+        System.out.println("一个星期中的天数：" + dateFormatter.format(date));
+
+        br.close();
+
+    }
+}
